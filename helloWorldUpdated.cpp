6b5fb2c1373b558fb13a9f85d61541ebd3886f01
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-15 04:50:43 +0000

    Made statements to Dr. Phil in CPP

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Dr. Phil!" << std::endl;
+    std::cout << "Did you know that you are bald?" << std::endl;
+    std::cout << "You are bald" << std::endl;
+}
